040788b30328897ebadb08d781d92cbcfc2b7bdf
Jose Meira <jose.meira@fba-consulting.com>
2025-10-07 18:43:02 +0200

    comeack to main menu after get information about phone

--- main.py
+++ main.py
@@ -31,19 +31,35 @@ def show_banner():
     time.sleep(0.5)
 
 def main():
-    show_banner()
-    
-    try:
-        track_phone()
-        input(f'\n{Wh}[ {Gr}+ {Wh}] {Gr} Press enter to exit')
-    except KeyboardInterrupt:
-        print(f'\n{Wh}[ {Re}! {Wh}] {Re}Exit')
-        time.sleep(2)
-        sys.exit(0)
-    except Exception as e:
-        print(f'\n{Wh}[ {Re}! {Wh}] {Re}Error: {e}')
-        time.sleep(2)
-        sys.exit(1)
+    while True:
+        show_banner()
+        
+        try:
+            track_phone()
+            
+            print(f'\n{Wh}[ {Gr}1 {Wh}] {Gr} Lookup another phone number')
+            print(f'{Wh}[ {Gr}0 {Wh}] {Gr} Exit')
+            
+            choice = input(f'\n{Wh}[ + ] {Gr} Select Option : {Wh}')
+            
+            if choice == '0':
+                print(f'\n{Wh}[ {Re}! {Wh}] {Re}Exit')
+                time.sleep(1)
+                sys.exit(0)
+            elif choice != '1':
+                print(f'\n{Wh}[ {Re}! {Wh}] {Re}Invalid option, returning to lookup...')
+                time.sleep(1)
+                
+        except KeyboardInterrupt:
+            print(f'\n{Wh}[ {Re}! {Wh}] {Re}Exit')
+            time.sleep(2)
+            sys.exit(0)
+        except Exception as e:
+            print(f'\n{Wh}[ {Re}! {Wh}] {Re}Error: {e}')
+            time.sleep(2)
+            choice = input(f'\n{Wh}[ + ] {Gr} Try again? (y/N): {Wh}')
+            if choice.lower() != 'y':
+                sys.exit(1)
 
 if __name__ == '__main__':
     try:
